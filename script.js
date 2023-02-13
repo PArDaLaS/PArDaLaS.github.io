8c6e0e2340140defde70d99266bbc9732af42e57
PArDaLaS <72930025+PArDaLaS@users.noreply.github.com>
2023-02-13 23:32:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -56,3 +56,57 @@ function flowers() {
     container.pivot.y = container.height / 2;
 
 }
+
+
+function createFallingFlowers() {
+  // Create a PixiJS application
+  const app = new PIXI.Application({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    backgroundColor: 0x1099bb,
+  });
+  document.body.appendChild(app.view);
+
+  // Load an image of a flower
+  PIXI.Loader.shared.add("flower", "flower.png").load(setup);
+
+  function setup() {
+    // Create an array to store the falling flowers
+    const flowers = [];
+
+    // Create a container to hold the flowers
+    const flowersContainer = new PIXI.Container();
+    app.stage.addChild(flowersContainer);
+
+    // Create a loop to add new flowers to the stage
+    app.ticker.add((delta) => {
+      // Create a new flower sprite
+      const flower = new PIXI.Sprite(PIXI.Loader.shared.resources.flower.texture);
+
+      // Set the flower's position and scale
+      flower.x = Math.random() * app.screen.width;
+      flower.y = -100;
+      flower.scale.x = flower.scale.y = 0.1 + Math.random() * 0.3;
+
+      // Add the flower to the stage and the flowers array
+      flowersContainer.addChild(flower);
+      flowers.push(flower);
+    });
+
+    // Create a loop to update the position of each flower
+    app.ticker.add((delta) => {
+      for (let i = 0; i < flowers.length; i++) {
+        const flower = flowers[i];
+        flower.y += 10;
+        flower.rotation += 0.1;
+
+        // Remove the flower if it goes off the bottom of the screen
+        if (flower.y > app.screen.height) {
+          flowersContainer.removeChild(flower);
+          flowers.splice(i, 1);
+          i--;
+        }
+      }
+    });
+  }
+}
